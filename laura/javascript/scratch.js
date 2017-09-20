0bd9d595f9aada1c22c51eabb0a737dbfda51afb
Laura Boetcher <g33klaura@gmail.com>
2017-09-20 15:54:03 -0400

    Add initial folder structure and project tasks to consider

---
+++ laura/javascript/scratch.js
@@ -0,0 +1,11 @@
+// STEPS TO CONSIDER ====================
+// 
+
+// [] App name
+// [] Register app name with Spotify API ~would like to handle Spotify
+// [] Do separate files for API functions?
+// [] Do Firebase linking on main app.js file?
+// [] Form set up to link to Firebase
+// [] logo, color scheme
+// [] mobile-first design
+// [] list of features (then can delegate and create branch names for each feature)
