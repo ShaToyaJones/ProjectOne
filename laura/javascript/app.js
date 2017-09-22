b0edb62c3be81d38d8cdc58b0845b0960e4b1111
Laura Boetcher <g33klaura@gmail.com>
2017-09-22 17:52:38 -0400

    Initial Firebase script to app.js

---
+++ laura/javascript/app.js
@@ -0,0 +1,14 @@
+// Will need this script tag in HTML******
+// <script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
+// <script>
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCCOld_3clEWG1PqcEi5ashd3oedI8f_EY",
+  authDomain: "meal-grooves.firebaseapp.com",
+  databaseURL: "https://meal-grooves.firebaseio.com",
+  projectId: "meal-grooves",
+  storageBucket: "meal-grooves.appspot.com",
+  messagingSenderId: "948740357477"
+};
+firebase.initializeApp(config);
