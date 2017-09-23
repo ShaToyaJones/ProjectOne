4e6b87beb0d4c966e9d991a088c8127c66d55057
Laura Boetcher <g33klaura@gmail.com>
2017-09-22 21:16:07 -0400

    Able to render images from call object on html

--- laura/javascript/spotify.js
+++ laura/javascript/spotify.js
@@ -16,6 +16,8 @@
 // FUNCTIONS ====================
 //
 
+// Q: Will want the ajax call in a function to call in the app.js script?
+
 
 
 
@@ -34,7 +36,7 @@ $(document).ready(function() {
     "url": "https://api.spotify.com/v1/browse/featured-playlists?limit=5",
     "method": "GET",
     "headers": {
-      "authorization": "Bearer BQA7pBK68ohtIbEtuD0JMHHG7Q_tL3kRrUoo4jfSu1lBFl0HInqJJUf4tSxx5yXAOCYLX3NeLYFgW7Mrr-Dx2X2tSLdYEsoqPD82aNBgmxkJGzlOrblcDS75_FzHWtm425Nst_QSsw",
+      "authorization": "Bearer BQABgiEpN2Yf_bU7d2uiRvhR1lyscGafzZhi9j0Ni1P_H5gFzbHr6igW4QUwokTWuyluKWJ8qnz13yRgJRBXSlQm92GVSfd_xQuUL7QrbpXt2AO8e34s9x4tkEjZQbwXRcO11Hzkjw",
       // "cache-control": "no-cache",
       // "postman-token": "64edfafe-4f2d-1f40-fb53-d9c8c6408139"
     }
@@ -55,17 +57,22 @@ $(document).ready(function() {
       for (var k = 0; k < results.length; k++) {
 
         var listDiv = $("<div>");
+
+        // This for now is adding the ID text to the page. Needed to test if I was drilling into the object or not  ~WORKS
         var listId = $("<p>").text("Playlist ID: " + results[k].id);
 
+        // Grabs image from ajax object and renders on page  ~WORKS
+        var listImg = $("<img>");
+        listImg.attr("src", results[k].images[0].url);
+
         listDiv.append(listId);
+        listDiv.append(listImg);
 
         $("#api-tester").append(listDiv);
-        
       };
     
-
   });
-
-
+  // ^^Closes AJAX call
 
 });
+// ^^Closes document-ready
