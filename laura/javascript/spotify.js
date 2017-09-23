239f9fe12391fd5a21cd0b42ebed92f4dba2b39e
Laura Boetcher <g33klaura@gmail.com>
2017-09-22 20:01:13 -0400

    Update auth for API, issues with called object

--- laura/javascript/spotify.js
+++ laura/javascript/spotify.js
@@ -1,4 +1,6 @@
+// Script specific to Spotify API calls
 
+// [] Link in project HTML
 
 // First Spotify API call to get list of featured playlists
 var featPlaylists = {
@@ -7,12 +9,68 @@ var featPlaylists = {
   "url": "https://api.spotify.com/v1/browse/featured-playlists?limit=5",
   "method": "GET",
   "headers": {
-    "authorization": "Bearer BQDc3bb-EM3Y9MJjYA3CobhT4mfHziPKaNQr0Xyh4N8F65tPRykuj-R218bN2OobboOhg8FiXTzbOIGGup-Zyw",
-    "cache-control": "no-cache",
-    "postman-token": "53b54575-1285-7bc4-941b-1599a3f5fedf"
+    "authorization": "Bearer BQA7pBK68ohtIbEtuD0JMHHG7Q_tL3kRrUoo4jfSu1lBFl0HInqJJUf4tSxx5yXAOCYLX3NeLYFgW7Mrr-Dx2X2tSLdYEsoqPD82aNBgmxkJGzlOrblcDS75_FzHWtm425Nst_QSsw",
+    // "cache-control": "no-cache",
+    // "postman-token": "64edfafe-4f2d-1f40-fb53-d9c8c6408139"
   }
 }
 
-$.ajax(featPlaylists).done(function (response) {
-  console.log(response);
+// $.ajax(featPlaylists).done(function (response) {
+//   console.log(response);
+// });
+
+// ISSUE - Tokens expire
+// New ISSUE - "Request header not allowed by Access-Control-Allow-Headers in preflight response" ~FIXED if comment-out cache-control & postman-token
+
+
+// VARIABLES ====================
+// 
+
+// var results = "";
+
+
+
+// FUNCTIONS ====================
+//
+
+
+
+
+// MAIN PROCESS ====================
+// 
+
+// Transfer this to app.js when ready
+// Render something from API object length
+
+$(document).ready(function() {
+
+
+  $.ajax(featPlaylists).done(function (response) {
+    
+    console.log("ajax response object = ");
+    console.log(response);
+
+    var results = response.data;
+
+      console.log("var results = " + results);
+
+      // RESULTS COMING UP UNDEFINED*****************
+
+    for (var featPlaylistsIndex = 0; featPlaylistsIndex < results.length; featPlaylistsIndex++) {
+
+    // var listDiv = $("<div>");
+    var listImg = $("<img>");
+
+    listImg.attr({
+      "src": results[featPlaylistsIndex].playlists.items.images.url,
+    });
+
+    $("#api-tester").append(listImg);
+
+    }
+
+  });
+
+
+
 });
