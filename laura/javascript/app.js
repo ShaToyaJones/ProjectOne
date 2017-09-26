1c40418857abcd06118e842c038bb31c2a3cae5f
Laura Boetcher <g33klaura@gmail.com>
2017-09-26 14:27:40 -0400

    Working out if else statement to control either music search term or playlist browse

--- laura/javascript/app.js
+++ laura/javascript/app.js
@@ -60,75 +60,92 @@ $(document).ready(function() {
 
     console.log("Submit was clicked!");
 
-    // Captures value of music input field
-    music = $("#music-search").val().trim().toLowerCase();
-    	// console.log(music);
 
-    // Captures value of ingredients input field
-    food = $("#enterIngredients").val().trim().toLowerCase();
-      // console.log(food);
+    // ######IF/ELSE STATEMENT HERE?
+    
+    if ($("#music-search").val().length !== 0) {
+      // IF MUSIC INPUT THEN SEARCH & SEARCH...
 
-    // Clears input fields on submit
-    $("#search-form").trigger("reset");
+      // Captures value of music input field
+      music = $("#music-search").val().trim().toLowerCase();
+        console.log(music);
 
-    // $("#playlist-covers").empty();
-    // ^^Need a way to clear out playlist covers on each search submit*************
+      // Captures value of ingredients input field
+      food = $("#enterIngredients").val().trim().toLowerCase();
+        console.log(food);
+
+      // Clears input fields on submit
+      $("#search-form").trigger("reset");
+
+      // $("#playlist-covers").empty();
+      // ^^Need a way to clear out playlist covers on each search submit*************
+
+      // Send values to Firebase 
+      // database.ref().push( {
+      // database.ref().set( {
+      //   musicSearch: music,
+      //   ingredientSearch: food,
+      //   dateAdded: firebase.database.ServerValue.TIMESTAMP
+      // });
+
+
+    } else {
+      // ELSE RUN FEATURED & SEARCH
+
+      // browseFeatured();
+      // ^^Keep function call outside of Firebase sync
+      // Not syncing Featured Playlist data to Fb
+
+      // Captures value of music input field
+      music = "browse";
+        console.log(music);
+
+      // Captures value of ingredients input field
+      food = $("#enterIngredients").val().trim().toLowerCase();
+        console.log(food);
+
+      // Clears input fields on submit
+      $("#search-form").trigger("reset");
+
+      // $("#playlist-covers").empty();
+      // ^^Need a way to clear out playlist covers on each search submit*************
+
+      // Send values to Firebase 
+      // database.ref().push( {
+      // database.ref().set( {
+      //   musicSearch: music,
+      //   ingredientSearch: food,
+      //   dateAdded: firebase.database.ServerValue.TIMESTAMP
+      // });
+
+    };
+    // ^^Closes if/else
+
+    // ####Should the firebase setting/callback be in a function to call at the end of each part of the if/else?????????
 
     // Send values to Firebase 
-    // database.ref().push( {
-    database.ref().set( {
-    	musicSearch: music,
-      ingredientSearch: food,
-    	dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+      database.ref().push( {
+      // database.ref().set( {
+        musicSearch: music,
+        ingredientSearch: food,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
 
-  });
-  // ^^Closes search-submit on-click
+      // Fb values ARE being set.....
 
-
-  // Handles "lucky search" button, which will still send ingredients search to Firebase but will render featured playlists
-  $("#browse-submit").on("click", function() {
-
-    event.preventDefault();
-
-    console.log("Browse submit was clicked!");
-
-    browseFeatured();
-    // ^^Keep function call outside of Firebase sync
-    // Not syncing Featured Playlist data to Fb
-
-    // Captures value of ingredients input field
-    food = $("#enterIngredients").val().trim().toLowerCase();
-      // console.log(food);
-
-    // Clears input fields on submit
-    $("#search-form").trigger("reset");
-
-    // $("#playlist-covers").empty();
-    // ^^Need a way to clear out playlist covers on each search submit*************
-
-    // Send values to Firebase 
-    // database.ref().push( {
-    database.ref().set( {
-      ingredientSearch: food,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-
-  });
-  // ^^Closes browse-submit function
-
-
-  database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+      // So ONLY calls back correctly when "push" enabled.... probably b/c ordering by child.....
+    database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
 
       var snap = snapshot.val();
-        console.log("You searched for: " + snap.musicSearch + " and " + snap.ingredientSearch);
+        console.log(snap);
+        // console.log("You searched for: " + snap.musicSearch + " and " + snap.ingredientSearch);
 
       ingredientSearchTerm = snap.ingredientSearch;
         console.log(ingredientSearchTerm);
 
       // Need Heather's function to search recipes to call here
-      // RECIPE SEARCH FUNCTION
-      searchRecipeByIngredients();
+      // RECIPE SEARCH FUNCTION **OFF WHILE TESTING FB
+      // searchRecipeByIngredients();
 
       // So..... how to capture snap.musicSearch so it can be used in the query URL for a playlist search cal....
       // Declare new global var of musicSearchTerm = "";
@@ -136,13 +153,43 @@ $(document).ready(function() {
 
       musicSearchTerm = snap.musicSearch;
         console.log(musicSearchTerm);
+        // ###### but if musicSearchTerm is empty b/c browsing..... how should this a) know, b) THEN call the browsing function??????************
 
-      searchPlaylists();
-      // ####### Functions are in weird places................................
-          // ####DOES NEED FIREBASE REPEATED HERE, SANS MUSIC SYNC
+      if (musicSearchTerm === "browse") {
+
+        browseFeatured();
+          console.log("browseFeatured called");
+
+      } else {
+
+        searchPlaylists();
+          console.log("searchPlaylists called");
+
+      };
+    });
+  // ^^Closes database.ref function
+
+  // ####TRY PUTTING FIREBASE STUFF IN IT'S OWN FUNCTION, THEN CALL IN EACH PART OF EARLIER IF/ELSE
 
   });
-  // ^^Closes database.ref function
+  // ^^Closes search-submit on-click
+
+
+  // Handles "lucky search" button, which will still send ingredients search to Firebase but will render featured playlists
+  // $("#browse-submit").on("click", function() {
+
+    // event.preventDefault();
+
+    // console.log("Browse submit was clicked!");
+
+    // #####grabbed browseing/searching code from here to put in if/else
+    
+
+  // });
+  // ^^Closes browse-submit function
+
+
+
 
 });
 // ^^Closes doc ready
