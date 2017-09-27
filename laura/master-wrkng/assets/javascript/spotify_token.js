94d2f83c4a5bd7f45e44eeebcaafed3a5a89430e
Laura Boetcher <g33klaura@gmail.com>
2017-09-27 14:35:01 -0400

    Create separate script for Spotify token to ease in updating

---
+++ laura/master-wrkng/assets/javascript/spotify_token.js
@@ -0,0 +1,8 @@
+// Script holds variable just for Spotify token
+
+
+// VARIABLES ====================
+// 
+
+var authToken = "Bearer BQAtbu7FUFu5_5TimvBK00tPlIRGETbM_Bjctxbelxmi_rwuYZKs_RpuNbgdTEZS67FurVfYhF_re9SiYhsu-A";
+// ^^Update this as needed until working on own
