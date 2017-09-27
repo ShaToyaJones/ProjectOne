1131d1694148533a52e7c0ea11003bb4bd0f1dd6
Heather Mathies <heathermathies@Heathers-MacBook-Air.local>
2017-09-26 22:39:33 -0400

    both calls to spoonacular working

--- Heather-Test/spoonacular-testing.js
+++ Heather-Test/spoonacular-testing.js
@@ -1,74 +1,78 @@
 /*----when the page loads*/
 $(document).ready(function() {
-    // Initial array of food----will need to change this to push to firebase
-    var food = [];
-   
-    //this needs to be changed to a firebase event(check with Laura)rather than push to array
-    // this function handles events where an add recipe button is clicked
     $('#addFood').on("click", function(event) {
         event.preventDefault();
 
-        $("#userDir").text("Click on an image to get the recipe");
+        console.log("I was clicked");
+        //Call the function that makes the request to the API
+        searchRecipeByIngredients();
+    });
 
-        //this line grabs the input from the textbox
-        var newRecipe = $('#foodInput').val().trim().toLowerCase();
-        //adding recipe from the textbox to our array
-        food.push(newRecipe);
-        console.log("new array: " + food);
+function searchRecipeByIngredients() {
+// $("#userDir").text("Click on an image to get the recipe");
+
+    // // sets newRecipe to the value of ingredientSearchTerm, which is returned from Firebase in app.js
+    // var newRecipe = ingredientSearchTerm;
+    
+    // // ####SEE IF THIS LOGS FIRST!!!!!!!!!!!!!  ~WORKS
+    // console.log("Ingredient search term is: " + ingredientSearchTerm);
+    //this line grabs the input from the textbox
+    var newRecipe = $('#foodInput').val().trim().toLowerCase();
+
+    // Constructing a queryURL using the user input stored in the newRecipe variable
+    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + newRecipe + "&limitLicense=false&number=4&ranking=1";
+
+    console.log(queryURL);
+        
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      headers: {
+        "X-Mashape-Key": "O8cILmkawTmsh422cdG88s3nvdobp1Mz7E0jsnq3EpxgBPdyg8",
+        "Accept": "application/json"
+      },
+        url: queryURL,
+        method: "GET"
+        
+    // After data comes back from the request
+    }).done(function(response) {
+      
+      $("#imageFood").html('');
             
+        console.log(response);
+        
+        // Looping through each API recipe result 
+        for (var i = 0; i < response.length; i++) {
+          // Creating and storing a div tag
+          var recipeDiv = $(" <div class='card-content'>");
 
-        // Constructing a queryURL using the user input stored in the newRecipe variable
-        var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + 
-        newRecipe + "&limitLicense=false&number=4&ranking=1";
+              // ^^^Wondering if this work better as just "<div>", then a separate .addClass for "card-content light-blue" #lcb
 
-        console.log(queryURL);
-        // Performing an AJAX request with the queryURL
-        $.ajax({
-            headers: {
-                "X-Mashape-Key": "O8cILmkawTmsh422cdG88s3nvdobp1Mz7E0jsnq3EpxgBPdyg8",
-                "Accept": "application/json"
-            },
-            url: queryURL,
-            method: "GET"
-                // After data comes back from the request
-        }).done(function(response) {
-            $("#imageFood").html('');
-            
-            console.log(response);
-            // Looping through each API recipe result 
-            for (var i = 0; i < response.length; i++) {
-                // Creating and storing a div tag
-                var recipeDiv = $(" <div class='card-content light-blue lighten-5'>");
+          // Creating and storing an image tag
+          var recipeImage = $("<img width=300px height=auto>");
+            //this adds the class food to all images
+            recipeImage.addClass("food");
+            // Setting the src attribute of the image to a property pulled off the result item
+            recipeImage.attr("src", response[i].image);
+            recipeImage.attr('data-id',response[i].id);
 
-                // Creating and storing an image tag
-                var recipeImage = $("<img width=300px height=auto>");
-                //this adds the class food to all images
-                recipeImage.addClass('img', food);
-                // Setting the src attribute of the image to a property pulled off the result item
-                recipeImage.attr("src", response[i].image);
-                recipeImage.attr('data-id',response[i].id);
+            console.log("This is the image: " + recipeImage);
 
-                console.log("This is the image: " + recipeImage);
-
-
-                var recTitle = $("<span class='card-title'>").text(response[i].title);
-
+            var recTitle = $("<span class='card-title'>").text(response[i].title);
                 
                 // put the recipe image in the recipe div
-                
-                recipeDiv.append(recTitle);
                 recipeDiv.append(recipeImage);
-                var recipeCard = $("<div class='card light-blue lighten-5'>").append(recipeDiv)
+                recipeDiv.append(recTitle);
+                var recipeCard = $("<div class='card'>").append(recipeDiv)
                 // display the recipe image to the html
                 $("#imageFood").append(recipeCard);
-
-            }
-        })
-    });
+            };
+        });
+        // ^^^Closes ajax1 done function
+};
     /*create an onclick function which calls the Get recipe information 
     create a link that will show step by step instructions, ingredient list, 
     the recipe title, image, and time to make*/
-    $("body").on("click", "img", function(event) {
+    $("body").on("click", ".food", function(event) {
         /*to make it switch to another page, location.href- #maybe add the id
         look into the id tag*/
         event.preventDefault();
@@ -103,10 +107,9 @@ $(document).ready(function() {
         var recipeClicked = $("<img width=800px height=auto>");
         var recipeImage = $("<img width=300px height=auto>");
         //this adds the class food to all images
-        recipeImage.addClass('img', food);
+        recipeImage.addClass("food");
         // Setting the src attribute of the image to a property pulled off the result item
         recipeImage.attr("src", data.image);
-        console.log(data);
         var recTitle = $("<span class='card-title'>").text(data.title);
         var ingredientList = '';
         for(i in data.extendedIngredients){
@@ -115,46 +118,29 @@ $(document).ready(function() {
         var ingredList = $("<p>").html("Ingredients: " + ingredientList);
         var timeToMake = $("<p>").text("Total Time: " + data.readyInMinutes);
         var servings = $("<p>").text("Serving Size: " + data.servings);
-        var directions = $("<p>").text("Directions: " + data.instructions);
+        var directions = $("<p>").text( data.instructions);
         //setting the src attribute to multiple properties pulled off the data from JSON
         // recipeClicked.addClass("img", recipe);
         recipeClicked.attr("src", data.image);
         
 
-        recipeDetailDiv.append(recTitle);
+        
         recipeDetailDiv.append(recipeImage);
+        recipeDetailDiv.append(recTitle);
         recipeDetailDiv.append(ingredList);
         recipeDetailDiv.append(timeToMake);
         recipeDetailDiv.append(servings);
         recipeDetailDiv.append(directions);
-        var recipeDetailCard = $("<div class='card light-blue lighten-5'>").append(recipeDetailDiv);
+        var recipeDetailCard = $("<div class='card'>").append(recipeDetailDiv);
         
         $("#imageFood").append(recipeDetailCard);
-        console.log(recTitle);
-        console.log(ingredList);
-        console.log(timeToMake);
-        console.log(servings);
-        console.log(directions);
 
    
     })
      });
 
-    });
+});
 
-/*
-<script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCrxXBy3T5g8zAgijTgYmc3C_H_QQmexlo",
-    authDomain: "meal-grooves-app.firebaseapp.com",
-    databaseURL: "https://meal-grooves-app.firebaseio.com",
-    projectId: "meal-grooves-app",
-    storageBucket: "",
-    messagingSenderId: "149220257930"
-  };
-  firebase.initializeApp(config);
-</script>*/
+
 
 
