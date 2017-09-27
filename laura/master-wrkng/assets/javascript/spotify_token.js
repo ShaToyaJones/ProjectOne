de40e1a7ab4d51641da9f2a3d5076e227dbf8ee9
Laura Boetcher <g33klaura@gmail.com>
2017-09-27 14:42:02 -0400

    Add fresh token. API calls still work

--- laura/master-wrkng/assets/javascript/spotify_token.js
+++ laura/master-wrkng/assets/javascript/spotify_token.js
@@ -4,5 +4,6 @@
 // VARIABLES ====================
 // 
 
-var authToken = "Bearer BQAtbu7FUFu5_5TimvBK00tPlIRGETbM_Bjctxbelxmi_rwuYZKs_RpuNbgdTEZS67FurVfYhF_re9SiYhsu-A";
+var authToken = "Bearer BQDutN2FBUB7IuQr5nMWWwp2AHY_azGq0sDbCtYO-NAOZo7WC_zGoXiXYnNsI_v0a0og_WOaHac3GbNQhGVwPw";
 // ^^Update this as needed until working on own
+// 2:37pm Wed 9/27
