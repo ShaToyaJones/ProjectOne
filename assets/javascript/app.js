86d368099702f9739e9508d60707d9dffac856b8
ShaToyaJones <jshatoyaj@yahoo.com>
2017-09-28 22:04:01 -0400

    Added code to clear submission form on Contact Us. Changed button color. Added image background.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,5 +5,9 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 	$('select').material_select();    
 	$('#textarea1').val("");
-	$('#textarea1').trigger('autoresize');
+	// $('#textarea1').textarea1rigger('autoresize');
+	// Clears input fields on submit
+	$(".btn").on("click", function(){
+		$("#contactForm").trigger("reset");
+	});
 });
